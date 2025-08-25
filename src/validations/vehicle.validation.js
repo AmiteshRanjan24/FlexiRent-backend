e160ba3c0ff9ed22024c560e113c977af1d7bcf2
Amitesh Ranjan <amiteshranjan5524@gmail.com>
2025-08-26 02:52:09 +0530

    added updateVehicleSchema

--- src/validations/vehicle.validation.js
+++ src/validations/vehicle.validation.js
@@ -1,12 +1,13 @@
 const z = require("zod");
 
 const vehicleSchema = z.object({
-  providerId: z.string().regex(/^[0-9a-fA-F]{24}$/, "Invalid providerId"),
-  vehicleCompany: z.string().min(2, "vehicle company is required"),
+  vehicleCompany: z.string().min(2, "Vehicle company is required"),
   type: z.enum(["car", "bike", "scooter", "other"]),
   model: z.string().min(1, "Model is required"),
   pricePerDay: z.number().positive("Price must be greater than 0"),
-  availability: z.boolean(),
+  availability: z.boolean().optional().default(true),
 });
 
-module.exports = vehicleSchema;
+const updateVehicleSchema = vehicleSchema.partial();
+
+module.exports = { vehicleSchema, updateVehicleSchema };
