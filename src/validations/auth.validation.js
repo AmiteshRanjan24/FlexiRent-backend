a123d5fac5b4ceb2c256d1f428b3e858ef0da82a
Amitesh Ranjan <amiteshranjan5524@gmail.com>
2025-08-25 22:11:36 +0530

    updated authSchema

--- src/validations/auth.validation.js
+++ src/validations/auth.validation.js
@@ -1,7 +1,7 @@
 const z = require("zod");
 
 const authSchema = z.object({
-  name: z.string().min(3),
+  name: z.string().min(3, "Name must be at least 3 characters long"),
   email: z.email("Invalid email address"),
   password: z.string().min(6, "Password must be atleast 6 characters long"),
   role: z.enum(["user", "provider"]).optional().default("user"),
